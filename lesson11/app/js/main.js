6ccb0310a6c399affcd4c9c92afc3e8dfc7f0cae
Arthur404 <ananarben@gmail.com>
2017-01-02 22:39:06 +0200

    section news added

--- lesson11/app/js/main.js
+++ lesson11/app/js/main.js
@@ -1,4 +1,4 @@
-
+//slider
 $('.slider-hero').slick({
     dots: true,
     arrows: false,
@@ -28,3 +28,59 @@ $('.slider-main').slick({
         }
     ]
 });
+
+//counters
+$(document).ready(function(){
+    $('.comment').click(function() {
+        $('.count-com').html(+$('.count-com').html()+1);
+        controlCom.progressIncrement();
+    });
+    $('.views').click(function() {
+        $('.count-view').html(+$('.count-view').html()+1);
+        controlView.progressIncrement();
+    });
+    $('.progress').progressInitialize();
+    var controlCom = $('.control'),
+    controlView = $('.control-view');
+    control.click(function(e){
+        e.preventDefault();
+    });
+});
+(function($){
+    $.fn.progressInitialize = function(){
+        return this.each(function(){
+            var button = $(this),
+                progress = 0;
+            var options = $.extend({
+                type:'background-horizontal'
+            }, button.data());
+            var bar = $('<span class="load ' + options.type + '">').appendTo(button);
+            button.on('progress', function(e, val, absolute){
+                if(!button.hasClass('in-progress')){
+                    bar.show('10');
+                    progress = 20;
+                    button.addClass('in-progress')
+                }
+                if(absolute){
+                    progress = val;
+                }
+                else{
+                    progress += val;
+                }
+                if(progress >= 100){
+                    progress = 100;
+                }
+                setProgress(progress);
+            });
+            function setProgress(percentage){
+                bar.width(percentage+'%');
+            }
+        });
+    };
+    $.fn.progressIncrement = function(val){
+        val = val || 5;
+        var button = this.first();
+        button.trigger('progress',[val])
+        return this;
+    };
+})($);
