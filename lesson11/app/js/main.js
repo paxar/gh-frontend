ba7ef968f807f711e5c0a449185870bfce831809
Arthur404 <ananarben@gmail.com>
2017-01-02 18:40:16 +0200

    section clients added

--- lesson11/app/js/main.js
+++ lesson11/app/js/main.js
@@ -1,6 +1,30 @@
 
-$('.slider').slick({
+$('.slider-hero').slick({
     dots: true,
     arrows: false,
     cssEase: 'ease-in'
+});
+
+$('.slider-main').slick({
+    dots: true,
+    arrows: false,
+    cssEase: 'ease-in',
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    responsive: [
+        {
+            breakpoint: 680,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        },
+        {
+            breakpoint: 992,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        }
+    ]
 });
