b02d93ea8485d81091a5542e54db1f936ffea937
Arthur404 <ananarben@gmail.com>
2017-01-26 21:38:06 +0200

    section hero added

---
+++ lesson14/Singular/app/js/main.js
@@ -0,0 +1,33 @@
+//slider
+$('.slider').slick({
+    dots: true,
+    arrows: false,
+    cssEase: 'ease-in'
+});
+
+$('.nav-menu').css({'height':'0'});
+
+$('#close-nav').click(function () {
+    $('.nav-menu').animate({'height':'0'},300)
+});
+
+$('#open-nav').click(function () {
+    $('.nav-menu').animate({'height':'100vh'},300)
+});
+
+$(window).scroll(function(){
+    if($(this).scrollTop()>1){
+        $('header').css({'background':'rgba(34,34,34,0.9)'});
+    }
+    else if ($(this).scrollTop()<1){
+        $('header').css({'background':'none'});
+    }
+});
+
+$('.scroll').click(function() {
+    $('html, body').animate({
+        scrollTop: $('.introduction').offset().top
+    }, 700);
+    return false;
+});
+
