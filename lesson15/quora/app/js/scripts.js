5c8c4204ca4a5a2bc7af392831402ed15e0e8de0
Pavel Charchenko <paxar81@gmail.com>
2017-02-02 16:10:25 +0200

    lesson15 quora   - add blog and pricing section + styled it

--- lesson15/quora/app/js/scripts.js
+++ lesson15/quora/app/js/scripts.js
@@ -16,6 +16,29 @@ $(document).ready(function () {
 });
 
 
+/*   Masonry    */
+
+$(document).ready(function(){
+    $('#masonry').masonry({
+// указываем элемент-контейнер в котором расположены блоки для динамической верстки
+        itemSelector: '.blog-item',
+// указываем класс элемента являющегося блоком в нашей сетке
+        singleMode: true,
+// true - если у вас все блоки одинаковой ширины
+        isResizable: true,
+// перестраивает блоки при изменении размеров окна
+        isAnimated: true,
+// анимируем перестроение блоков
+
+        animationOptions: {
+            queue: false,
+            duration: 500
+        }
+// опции анимации - очередь и продолжительность анимации
+    });
+});
+
+
 
 
 
--- lesson15/quora/gulpfile.js
+++ lesson15/quora/gulpfile.js
@@ -47,6 +47,7 @@ gulp.task('scripts', function() {
         'app/libs/jquery/dist/jquery.min.js', // Берем jQuery
         'app/libs/bootstrap-sass/assets/javascripts/bootstrap.min.js',// Берем bootstrap
         'app/libs/isotope/dist/isotope.pkgd.min.js',// Берем isotope
+        'app/libs/masonry/dist/masonry.pkgd.min.js',// Берем masonry
         'app/js/scripts.js' //Берем свои скрипты для страниц
     ])
         .pipe(concat('libs.min.js')) // Собираем их в кучу в новом файле libs.min.js
