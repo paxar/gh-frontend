a7daab85692199e45f9e9b375356b0ce85f547a7
Pavel Charchenko <paxar81@gmail.com>
2017-02-03 16:52:31 +0200

    lesson15 quora   - finish beta...

--- lesson15/quora/app/js/scripts.js
+++ lesson15/quora/app/js/scripts.js
@@ -15,6 +15,23 @@ $(document).ready(function () {
     });
 });
 
+$(document).ready(function () {
+    $("#header").on("click", "a", function (event) {
+        event.preventDefault();
+        var id = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 700);
+    });
+});
+
+$(document).ready(function () {
+    $("#footer").on("click", "a", function (event) {
+        event.preventDefault();
+        var id = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 700);
+    });
+});
 
 /*   Masonry    */
 
